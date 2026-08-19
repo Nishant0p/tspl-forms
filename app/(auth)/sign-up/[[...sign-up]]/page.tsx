dc514a91e70e993cb8c6469e820bec7ec811bb1c
alpha-aks <149251160+alpha-aks@users.noreply.github.com>
2026-08-19 13:07:11 +0530

    fix: update Clerk SignUp redirect prop

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from '@clerk/nextjs';
 export default function SignUpPage() {
   return (
     <div>
-      <SignUp afterSignUpUrl={'/dashboard'} />
+      <SignUp fallbackRedirectUrl={'/dashboard'} />
     </div>
   );
 }
